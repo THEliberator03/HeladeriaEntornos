6869e3991bc203fb4a22e5982bfc5e6669e2316e
OteloxESP <antonioeelvento0123@gmail.com>
2022-04-26 16:44:23 +0200

    Cambios del examen

--- main.java
+++ main.java
@@ -5,17 +5,19 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String args[]) {
+        double precio = 0;
         System.out.println("¿Quieres un cono o una tarrina?");
         String helado = new Scanner(System.in).nextLine();
         System.out.println("Los sabores que tenemos son los siguientes:");
         System.out.println("fresa, nata, choco, naranja y limón");
-        String sabores[] = {" ", " f", "f "};
+        System.out.println("Tenemos tambien pistacho como nuevo sabor");
+        String sabores[] = {" ", " ", " "};
 
         if (helado.equalsIgnoreCase("cono")) {
+            precio = 2;
             for (int i = 0; i < 3; i++) {
                 System.out.println("¿Que sabor quiere?");
                 String sabor = new Scanner(System.in).nextLine();
-
                 switch (sabor) {
                     case "fresa":
                         sabores [i] = "fresa";
@@ -32,6 +34,8 @@ public class main {
                     case "limon":
                         sabores[i] = "limon";
                         break;
+                    case "pistacho":
+                        sabores[i] = "pistacho";
                     default:
                         System.out.println("No tenemos ese sabor");
                         i--;
@@ -45,9 +49,22 @@ public class main {
                 }
                 
             }
-            helado h1 = new cono(sabores[0],sabores[1],sabores[2]);
+            System.out.println("Su helado cuesta "+precio+" ¿Con cuanto vas a pagar?");
+            boolean rep = true;
+            while (rep){
+                double respuestaPaga = new Scanner(System.in).nextDouble();
+                if (respuestaPaga > precio){
+                    double resultado = respuestaPaga - precio;
+                    System.out.println("Aqui tiene su cambio ("+resultado+") y su helado");
+                    rep = false;
+                }else{
+                    System.out.println("No es suficiente dinero o no tiene suficiente saldo en la tarjeta, vuelva a intentarlo");
+                }
+            }
+            //helado h1 = new cono(sabores[0],sabores[1],sabores[2]);
             
         }else{
+            precio = 1.5;
             for (int i = 0; i < 3; i++) {
                 System.out.println("¿Que sabor quiere?");
                 String sabor = new Scanner(System.in).nextLine();
@@ -77,10 +94,24 @@ public class main {
                     String respuesta = new Scanner (System.in).nextLine();
                     if (respuesta.equalsIgnoreCase("No")){
                     i = 4;
+                    }else{
+                        precio +=0.5;
                     }
                 }
             }
-            helado h1 = new tarrina(sabores[0],sabores[1],sabores[2]);
+            System.out.println("Su tarrina cuesta "+precio+" ¿Con cuanto vas a pagar?");
+            boolean rep = true;
+            while (rep){
+                double respuestaPaga = new Scanner(System.in).nextDouble();
+                if (respuestaPaga > precio){
+                    double resultado = respuestaPaga - precio;
+                    System.out.println("Aqui tiene su cambio ("+resultado+") y su helado");
+                    rep = false;
+                }else{
+                    System.out.println("No es suficiente dinero o no tiene suficiente saldo en la tarjeta, vuelva a intentarlo");
+                }
+            }
+            //helado h1 = new tarrina(sabores[0],sabores[1],sabores[2]);
         }
     }
 }
